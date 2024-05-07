52e38edb84863f8debeb37c0ff37a79b94ba5590
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-07 20:50:06 +0000

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" FIFO cache
+"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO cache
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key = next(iter(self.cache_data))
+            del self.cache_data[oldest_key]
+            print("DISCARD: {}\n".format(oldest_key))
+
+    def get(self, key):
+        """get the value of the key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
