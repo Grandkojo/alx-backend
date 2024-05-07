7e6559b8dcc2a2677b03f338e5851b0c47390f18
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-07 20:32:25 +0000

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Basic Caching
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic dictionary"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """assign key to value"""
+        if key is None  or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get the value of the key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
