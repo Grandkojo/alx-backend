d64225998398a62358f125b0bbcb6ad1569914f6
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-16 12:23:20 +0000

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,15 +6,26 @@ from typing import Text
 from flask import Flask, render_template
 from flask_babel import Babel
 
-
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """The babel config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 
 
 @app.route('/')
 def index() -> Text:
     """return the index page of the app
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
