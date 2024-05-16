3d5bc1e4d84f1fa9bfb9ee4d71ce72fd471bb82a
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-16 17:31:11 +0000

    i18n

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""the flask app module"""
+
+
+from typing import (
+    Text,
+    Optional
+)
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+from flask import _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """The babel config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """get the locale from the request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> Text:
+    """return the index page of the app
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
