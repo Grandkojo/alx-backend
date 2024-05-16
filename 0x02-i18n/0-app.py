f3ed5353cf5cbe82e1f779fe2cf6d2869bf6af8b
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-16 12:02:03 +0000

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,14 @@
 """the flask app module"""
 
 
+from typing import Text
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    """return the index page of the app"""
+def index() -> Text:
+    """return the index page of the app
+    """
     return render_template('0-index.html')
