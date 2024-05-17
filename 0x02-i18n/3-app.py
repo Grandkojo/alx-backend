3d48e26ba1abc4320706918bfdff892b24bd9067
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-17 11:33:22 +0000

    i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,45 +1,53 @@
 #!/usr/bin/env python3
-"""the flask app module"""
+"""This is the flask app file"""
 
-
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g,
+)
+from flask_babel import Babel
 from typing import (
     Text,
     Optional
 )
-from flask import Flask, render_template
-from flask_babel import Babel
-from flask import request
-from flask_babel import _
-
-
-# @babel.localeselector
-def get_locale() -> Optional[str]:
-    """get the locale from the request
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
 
 
 class Config:
-    """The babel config class
+    """This is the config class for the flask app
     """
-    LANGUAGES = ["fr", "en"]
-    BABEL_DEFAULT_LOCALE = "fr"
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index() -> Text:
-    """return the index page of the app
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """The function that gets the locale
+
+    Returns:
+        Optional[str]: The locale
     """
-    return render_template('3-index.html')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+@app.route("/")
+def home() -> Text:
+    """This is the home route
+
+    Returns:
+        Text: The html template file
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
